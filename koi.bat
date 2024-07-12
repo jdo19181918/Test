b235c80ff7d8eb9303b2a67d25cc6280684fa4eb
jdo19181918 <jdoe19181918@gmail.com>
2024-07-12 16:09:34 -0600

    Update koi.bat

--- koi.bat
+++ koi.bat
@@ -1,1 +1,1 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri https://github.com/jdo19181918/Test/blob/main/koi.ps1 -OutFile Desktop\koi.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri https://github.com/jdo19181918/Test/blob/main/koi.ps1 -OutFile koi.ps1"
